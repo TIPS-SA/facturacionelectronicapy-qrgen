d1c555406205be30bce73fa2bcf3667b9bfc7442
Marcos Jara <adrianjara@gmail.com>
2021-10-05 17:17:21 -0300

    Limpieza

--- src/QRGen.ts
+++ src/QRGen.ts
@@ -12,7 +12,6 @@ class QRGen {
     generateQR(xml: string, env: "test" | "prod") {
         
         return xml2js.parseStringPromise(xml).then(obj => {
-            console.dir(obj);
 
             if (!(obj && obj['rDE'] && obj['rDE']['Signature'] && obj['rDE']['Signature'])) {
                 throw new Error("XML debe estar firmado digitalmente");
@@ -27,39 +26,39 @@ class QRGen {
             
             const nVersion = obj['rDE']['dVerFor'][0];
 
-            qr += "nVersion=" + nVersion + "&amp;";
+            qr += "nVersion=" + nVersion + "&";
 
             const id = obj['rDE']['DE'][0]['$']['Id'];
-            qr += "Id=" + id + "&amp;";
+            qr += "Id=" + id + "&";
 
             let dFeEmiDE = obj['rDE']['DE'][0]['gDatGralOpe'][0]['dFeEmiDE'][0];
             dFeEmiDE = Buffer.from(dFeEmiDE, 'utf8').toString('hex');
-            qr += "dFeEmiDE=" + dFeEmiDE + "&amp;";
+            qr += "dFeEmiDE=" + dFeEmiDE + "&";
 
             let dRucRec = "";
             if (obj['rDE']['DE'][0]['gDatGralOpe'][0]['gDatRec'][0]['iNatRec'][0] == 1) {
                 dRucRec = obj['rDE']['DE'][0]['gDatGralOpe'][0]['gDatRec'][0]['dRucRec'][0];
-                qr += "dRucRec=" + dRucRec + "&amp;";
+                qr += "dRucRec=" + dRucRec + "&";
             } else {
                 dRucRec = obj['rDE']['DE'][0]['gDatGralOpe'][0]['gDatRec'][0]['dNumIDRec'][0];
-                qr += "dRucRec=" + dRucRec + "&amp;";
+                qr += "dRucRec=" + dRucRec + "&";
             }
             
             const dTotGralOpe = obj['rDE']['DE'][0]['gTotSub'][0]['dTotGralOpe'][0];
-            qr += "dTotGralOpe=" + dTotGralOpe + "&amp;";
+            qr += "dTotGralOpe=" + dTotGralOpe + "&";
             
             const dTotIVA = obj['rDE']['DE'][0]['gTotSub'][0]['dTotIVA'][0];
-            qr += "dTotIVA=" + dTotIVA + "&amp;";
+            qr += "dTotIVA=" + dTotIVA + "&";
 
             const cItems = obj['rDE']['DE'][0]['gDtipDE'][0]['gCamItem'][0].length;
-            qr += "cItems=" + cItems + "&amp;";
+            qr += "cItems=" + cItems + "&";
             
             let digestValue = obj['rDE']['Signature'][0]['SignedInfo'][0]['Reference'][0]['DigestValue'][0];
             digestValue = Buffer.from(digestValue, 'utf8').toString('hex');
-            qr += "DigestValue=" + digestValue + "&amp;";
+            qr += "DigestValue=" + digestValue + "&";
 
             const idCSC = "001";
-            qr += "IdCSC=" + idCSC + "&amp;";
+            qr += "IdCSC=" + idCSC + "&";
 
             const valueForHash = nVersion + id + dFeEmiDE + dRucRec + dTotGralOpe + dTotIVA + cItems + digestValue + idCSC;
             let valueHashed = sha256(valueForHash);
@@ -68,8 +67,10 @@ class QRGen {
             qr += "cHashQR=" + valueHashed;
 
             console.log(qr);
-            obj['rDE']['gCamFuFD']['dCarQR'] = qr.replace(/amp;amp;/g, 'amp;');
-
+            //obj['rDE']['gCamFuFD']['dCarQR'] = qr.replace(/amp;amp;/g, 'amp;');
+            obj['rDE']['gCamFuFD']['dCarQR'] = {
+                _ : qr
+            };
             var builder = new xml2js.Builder();
             var xmlWithQR = builder.buildObject(obj);
             return xmlWithQR;
